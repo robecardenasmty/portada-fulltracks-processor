1e70b105585fc587343f0a7503b960e1223837bf
robecardenasmty <robecardenas@gmail.com>
2026-07-07 13:07:15 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb',
-    },
-  },
-  serverRuntimeConfig: {
-    maxBodySize: '100mb',
-  },
 }
 
 module.exports = nextConfig
